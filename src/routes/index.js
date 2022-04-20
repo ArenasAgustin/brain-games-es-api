c7dce13b3e58302c9dc4e17dca29a07daa775191
“ArenasAgustin” <arenasagustin7@gmail.com>
2022-04-20 13:12:14 -0300

    C:/Program Files/Git/message

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,12 @@ const { Router } = require("express");
 const router = Router();
 const scoreRoute = require("./score");
 
+router.use("/score", scoreRoute);
+
 router.use("/", (req, res) =>
   res.status(200).send({
     message: "Example project did not give you access to the api web services",
   })
 );
-
-router.use("/score", scoreRoute);
 
 module.exports = router;
