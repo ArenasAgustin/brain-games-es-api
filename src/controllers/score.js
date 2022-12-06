4866246596a5cc1b09d5afe7b05e886e093d96cd
“ArenasAgustin” <arenasagustin7@gmail.com>
2022-12-05 23:40:05 -0300

    add regex

--- src/controllers/score.js
+++ src/controllers/score.js
@@ -1,6 +1,6 @@
 const Score = require("../models/score");
 
-const getScores = async (req, res) => {
+const getScores = async (_, res) => {
   try {
     const scores = await Score.find();
 
@@ -13,24 +13,29 @@ const getScores = async (req, res) => {
 const setScores = async (req, res) => {
   const { name, scorePoints } = req.body;
 
-  let score = {};
-  const scoreAux = await Score.findOne({ name, scorePoints });
+  if (/^[a-zA-Z0-9 ]+$/.test(name) && /^[0-9]+$/.test(scorePoints)) {
+    let score = {};
+    const scoreAux = await Score.findOne({ name, scorePoints });
 
-  if (scoreAux)
-    return res.status(200).json({ message: "Score already exists" });
+    if (scoreAux)
+      return res.status(200).json({ message: "Score already exists" });
 
-  try {
-    score = new Score({
-      name,
-      scorePoints,
-    });
+    try {
+      score = new Score({
+        name,
+        scorePoints,
+      });
 
-    score.save();
+      score.save();
 
-    return res.status(200).json({ message: "Added Succefully", ...score });
-  } catch (err) {
-    return res.status(400).json({ message: "Cannot set the score", err });
-  }
+      return res.status(200).json({ message: "Added Succefully", ...score });
+    } catch (err) {
+      return res.status(400).json({ message: "Cannot set the score", err });
+    }
+  } else
+    return res
+      .status(400)
+      .json({ message: "The data is not of the required type", status: 400 });
 };
 
 module.exports = {
